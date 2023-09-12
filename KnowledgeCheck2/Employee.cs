26cfe34d137213b87f8f3c81d3bb21e09fb2e81d
Sabrina Clore <sabrinaclore@gmail.com>
2023-09-12 13:12:57 -0400

    Employee Class

---
+++ KnowledgeCheck2/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    public class Employee
+    {
+        public string? FirstName;
+        public string? LastName;
+        public string? Department;
+        public int YearsService;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hi " + FirstName + " from " + Department);
+        }
+
+    }
+}
---
+++ KnowledgeCheck2/Program.cs
@@ -0,0 +1,16 @@
+﻿using KnowledgeCheck2;
+using System.IO.Pipes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      Employee employee = new Employee();
+        employee.FirstName = "Sabrina";
+        employee.LastName = "Clore";
+        employee.Department = "Human Resources";
+        employee.YearsService = 10;
+
+        employee.Greeting();
+}
+}
